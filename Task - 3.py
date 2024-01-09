e93f550dac6520f3b2647365c1fd3bc8e260364b
Atahan <atahan11951195@gmail.com>
2024-01-09 20:44:35 +0400

    fix task 3

--- Task - 3.py
+++ Task - 3.py
@@ -1,81 +1,43 @@
-# Task: 3 Создайте класс "Правильная Дробь" и реализуйте методы сравнения, сложения,
-# вычитания и умножения для экземпляров этого класса. Не забудьте про сокращение дроби.
-from math import gcd
+class PackOfMoney:
+    def __init__(self, ):
+        self.bills = {}
 
+    def add_bill(self, n, c):
+        if n in self.bills:
+            self.bills[n] = self.bills[n] + c
+        else:
+            self.bills[n] = c
 
-class ProperFraction:
-    def __init__(self, numerator, denominator):
-        if denominator == 0:
-            raise ValueError("Denominator cannot be zero.")
-        common_factor = gcd(numerator, denominator)
-        self.numerator = numerator // common_factor
-        self.denominator = denominator // common_factor
+    def calculate_total(self):
+        total = 0
+        for n, c in self.bills.items():
+            total += n * c
+        return total
 
     def __str__(self):
-        return f"{self.numerator}/{self.denominator}"
-
-    def __eq__(self, other):
-        if not isinstance(other, ProperFraction):
-            raise TypeError("Unsupported type for equality comparison.")
-        return self.numerator == other.numerator and self.denominator == other.denominator
-
-    def __lt__(self, other):
-        if not isinstance(other, ProperFraction):
-            raise TypeError("Unsupported type for less than comparison.")
-        return self.numerator * other.denominator < other.numerator * self.denominator
-
-    def __le__(self, other):
-        return self == other or self < other
+        result = 'Pack of money: [ '
+        for n in self.bills.keys():
+            result += f'{n}$: {self.bills[n]}, '
+        result += ']'
+        return result
 
     def __add__(self, other):
-        if not isinstance(other, ProperFraction):
-            raise TypeError("Unsupported type for addition.")
-        new_numerator = self.numerator * other.denominator + other.numerator * self.denominator
-        new_denominator = self.denominator * other.denominator
-        return ProperFraction(new_numerator, new_denominator)
-
-    def __sub__(self, other):
-        if not isinstance(other, ProperFraction):
-            raise TypeError("Unsupported type for subtraction.")
-        new_numerator = self.numerator * other.denominator - other.numerator * self.denominator
-        new_denominator = self.denominator * other.denominator
-        return ProperFraction(new_numerator, new_denominator)
-
-    def __mul__(self, other):
-        if not isinstance(other, ProperFraction):
-            raise TypeError("Unsupported type for multiplication.")
-        new_numerator = self.numerator * other.numerator
-        new_denominator = self.denominator * other.denominator
-        return ProperFraction(new_numerator, new_denominator)
-
-    def reduce_fraction(self):
-        common_factor = gcd(self.numerator, self.denominator)
-        self.numerator //= common_factor
-        self.denominator //= common_factor
-        return self
+        for n in other.bills.keys():
+            c = other.bills[n]
+            self.add_bill(n, c)
 
 
-if __name__ == "__Task - 3__":
+pack_1 = PackOfMoney()
+pack_1.add_bill(50, 10)
+pack_1.add_bill(100, 5)
+pack_1.add_bill(10, 100)
+print(pack_1)
 
-    fraction1 = ProperFraction(3, 5)
-    fraction2 = ProperFraction(2, 3)
+pack_2 = PackOfMoney()
+pack_2.add_bill(50, 10)
+pack_2.add_bill(100, 5)
+pack_2.add_bill(100, 14)
+print(pack_2)
 
-    print("Fraction 1:", fraction1)
-    print("Fraction 2:", fraction2)
-
-    sum_fraction = fraction1 + fraction2
-    print("Sum:", sum_fraction)
-
-    difference_fraction = fraction1 - fraction2
-    print("Difference:", difference_fraction)
-
-    product_fraction = fraction1 * fraction2
-    print("Product:", product_fraction)
-
-    reduced_fraction1 = fraction1.reduce_fraction()
-    reduced_fraction2 = fraction2.reduce_fraction()
-
-    print("Reduced Fraction 1:", reduced_fraction1)
-    print("Reduced Fraction 2:", reduced_fraction2)
-
-
+pack_1 + pack_2
+print(f'Total sum: {pack_1.calculate_total()}')
